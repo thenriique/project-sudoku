8fc29cea14a6ed0380e727b2fa0fb920ff13cf8e
Thiago Henrique <thiago.hrvieira@gmail.com>
2021-03-16 23:48:47 -0300

    Add algorithm aStar

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # project-sudoku
 This project has propouse to solve the "sudoku" game through search methods.
+
+For compile execute "python sudoku.py <algorithm> <archive_input>"
+
+Chosen build: "python sudoku.py dfs input.txt"
+
+    algorithm options
+        - bfs
+        - dfs
+        - aStar
+
+    archive_input
+        - default: input.txt
+
+## Versions
+Python: 3.9.1
+
+## Libraries
--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,8 @@
 ## Imports
 import copy
+import sys
 from datetime import datetime
+
 
 ## Functions
 def openArchive(inputs = 'input.txt'): # Tratamento para a abertura do arquivo de entrada
@@ -22,7 +24,12 @@ def generateMatriz(sudoku): # Gerador da grade do sudoku
 
     i = 0
     for item in sudoku: # Percorre cada caractere
-        line.append(item)
+        if item == '\n':
+            continue
+        elif item == '.' :
+            line.append(0)
+        else:
+            line.append(int(item))
 
         if len(line) == 9: # Caso já tenha 9 caracteres, insere na grade e limpa o vetor 'line'
             grid.append(line)
@@ -52,6 +59,21 @@ def nextVertice(vertice):
     else:
         return (vertice[0] + 1, 0)
 
+def firstVertice(sudoku):
+    i1 = 0
+    i2 = 0
+
+    for row in sudoku:
+        for column in row:
+            if column == 0:
+                return (i1, i2)
+            
+            if i2 == 8 :
+                i1 = i1 + 1
+                i2 = 0
+            else:
+                i2 = i2 + 1
+
 def nextPosition(position):
     if position < 89:
         return position + 1
@@ -59,18 +81,14 @@ def nextPosition(position):
         return -1
 
 def numberValids(sudoku, vertice):
-    numbers_valids = ['1','2','3','4','5','6','7','8','9']
-
-    # Validação de item preenchido
-    if sudoku[vertice[0]][vertice[1]] != '.':
-        return '0'
+    numbers_valids = [1,2,3,4,5,6,7,8,9]
     
     for item in list(set(sudoku[vertice[0]])): # Itens preenchidos na linha selecionada
-        if item != '.' and item in numbers_valids:
+        if item != 0 and item in numbers_valids:
             numbers_valids.remove(item)
 
     for item in sudoku: # Itens preenchidos na coluna selecionada
-        if item[vertice[1]] != '.' and item[vertice[1]] in numbers_valids:
+        if item[vertice[1]] != 0 and item[vertice[1]] in numbers_valids:
             numbers_valids.remove(item[vertice[1]])
 
     x0 = ((int)(vertice[0] / 3)) * 3
@@ -78,10 +96,26 @@ def numberValids(sudoku, vertice):
     for a in range(0,3): # Itens preenchidos na grade (3x3)
         for b in range(0,3):
             item = sudoku[x0 + a][y0 + b]
-            if item != '.' and item in numbers_valids:
+            if item != 0 and item in numbers_valids:
                 numbers_valids.remove(item)
     
     return numbers_valids
+
+def stateFinish(sudoku):
+    iDistante = 0
+
+    for row in sudoku:
+        for column in row:
+            if column == 0:
+                iDistante = iDistante + 1
+
+    return iDistante
+
+def heuristic(sudoku, vertice):
+    costFisish = stateFinish(sudoku)
+    possibilits = 9 - len(numberValids(sudoku, vertice))
+
+    return costFisish + possibilits
 
 def bfs(sudoku, vertice): # Função de Busca por Largura 
     fila = []
@@ -93,7 +127,7 @@ def bfs(sudoku, vertice): # Função de Busca por Largura
         if vertice == (): # Finaliza busca e apresenta o primeiro resultado
             break
 
-        if first[vertice[0]][vertice[1]] != '.': # Verifica se a posição já esta preenchida
+        if first[vertice[0]][vertice[1]] != 0: # Verifica se a posição já esta preenchida
             vertice = nextVertice(vertice)
             continue
 
@@ -106,58 +140,51 @@ def bfs(sudoku, vertice): # Função de Busca por Largura
 
     print(fila)
 
-
-# def bfs(sudoku, vertice): # Função de Busca por Largura 
-#     if vertice == (): # Finaliza busca e apresenta o primeiro resultado
-#         printSudoku(sudoku)
-#         return
-
-#     if vertice == (0,0): # Preenche arquivo com o retorno do primeiro vertice
-#         archive = openArchiveMemory('w')
-#         for item in numberValids(sudoku, vertice):
-#             sudoku[vertice[0]][vertice[1]] = item
-#             archive.write(generateLine(sudoku))
-#         archive.close()
-#         bfs(sudoku, nextVertice(vertice))
-#         return
-
-#     fila = list()
-#     archive_read = openArchiveMemory() # Le os registros do arquivo de memoria
-#     fila.append(archive_read.readlines())
-#     fila = fila[0]
-#     archive_read.close()
-
-#     if(generateMatriz(fila[0])[vertice[0]][vertice[1]] != '.'): # Valida se já existe registro nessa posição
-#         bfs(generateMatriz(fila[0]), nextVertice(vertice))
-#         return
-
-#     archive_write = openArchiveMemory('w')
-#     matriz = []
-#     for sudoku in fila: # Abre os filhos dos registros
-#         sudoku = generateMatriz(sudoku)
-#         for item in numberValids(sudoku, vertice):
-#             if item != '0':
-#                  sudoku[vertice[0]][vertice[1]] = item
-#                  matriz = generateLine(sudoku)
-#             archive_write.write(matriz) # Escreve os filhos no arquivo de memória
-#     archive_write.close()
-    
-#     bfs(generateMatriz(matriz), nextVertice(vertice)) # Vai para o próximo vertice
-
-
 def dfs(sudoku, vertice):
     if vertice == (): # Finaliza busca e apresenta o primeiro resultado
         printSudoku(sudoku)
         return True
     
-    if sudoku[vertice[0]][vertice[1]] != '.': # Verifica se a posição já esta preenchida
+    if sudoku[vertice[0]][vertice[1]] != 0: # Verifica se a posição já esta preenchida
         return dfs(sudoku, nextVertice(vertice))
         
     for item in numberValids(sudoku, vertice): # Percorre a fila
         sudoku[vertice[0]][vertice[1]] = item
         if dfs(sudoku, nextVertice(vertice)) == True:
             return True
-        sudoku[vertice[0]][vertice[1]] = '.'
+        sudoku[vertice[0]][vertice[1]] = 0
+
+
+
+def aStar(sudoku, vertice):
+    _list = []
+    _visited = []
+
+    _list.append({"node" : copy.deepcopy(sudoku), "nodeFather" : 0, "cost" : heuristic(sudoku, vertice)})
+
+    while _list:
+        first = _list[0]
+        v = firstVertice(first["node"])
+        
+        if stateFinish(first["node"]) == 0:
+            return first
+
+        for item in numberValids(first["node"], v):
+            aux = copy.deepcopy(first["node"])
+            aux[v[0]][v[1]] = item
+
+            if aux in _visited:
+                continue
+
+            if aux not in _list:
+                _list.append({"node" : copy.deepcopy(aux), "nodeFather" : copy.deepcopy(first), "cost" : heuristic(aux, nextVertice(v))})
+
+        _list.remove(first)
+        _list.sort(key=lambda x: x.get('cost'))
+        _visited.append(copy.deepcopy(first))
+        
+
+
 
 ## Methods
 def printSudoku(sudoku):
@@ -167,7 +194,7 @@ def printSudoku(sudoku):
     inline = ''
     for row in sudoku:
         for column in row:
-            inline += column
+            inline += str(column)
 
     print(inline)
 
@@ -179,35 +206,56 @@ def main():
     global debug
     grids = [] 
 
-    # Carregando registros do arquivo de entrada
-    archive = openArchive()
+    print (sys.argv)
 
-    # Montando a grade 
-    for line in archive:
-        grids.append(generateMatriz(line))
+    try:
+        # Carregando registros do arquivo de entrada
+        archive = ''
 
-    # Fechando o arqivo de entrada
-    archive.close()
+        if len(sys.argv) == 3 :
+            archive = openArchive(sys.argv[2])
+        else: 
+            archive = openArchive()
+
+        # Montando a grade 
+        for line in archive:
+            grids.append(generateMatriz(line))
+
+        # Fechando o arqivo de entrada
+        archive.close()
+    except:
+        print('Falha no carregamento do arquivo.')
+
 
     # Verifica se tem registros
     if len(grids) == 0:
-        print('Arquivo vazio! Por favor insira registros.\n')
+        print('Arquivo vazio! Por favor insira registros.')
         exit()
 
-    # Buscando por Largura
-    for grid in grids:
-        if(debug == True):
-            print(datetime.now(), ':', 'Iniciando busca')
-        bfs(grid, (0,0))   
-
-    # Busca por profundidade
-    # for grid in grids:
-    #     if(debug == True):
-    #         print(datetime.now(), ':', 'Iniciando busca')
-    #     dfs(grid, (0,0))           
+    
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'dfs': # Busca por Profundidade
+            for grid in grids:
+                if(debug == True):
+                    print(datetime.now(), ':', 'Iniciando busca')
+                dfs(grid, (0,0)) 
+        elif sys.argv[1] == 'bfs': # Buscando por Largura
+            for grid in grids:
+                if(debug == True):
+                    print(datetime.now(), ':', 'Iniciando busca')
+                bfs(grid, (0,0)) 
+        elif sys.argv[1] == 'aStar': # Busca A*
+            for grid in grids:
+                if(debug == True):
+                    print(datetime.now(), ':', 'Iniciando busca')
+                aStar(grid, (0,0)) 
+        else:
+            print('Falha no algoritmo definido.')  
+    else:
+        print('Algoritmo não especificado.')  
 
 ## Globals Variable
-debug = True
+debug = False
 
 ## Init
 main()
